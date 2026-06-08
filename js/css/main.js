9b21b303fc10524872ccb6265ebea968f283d833
awathiam3 <awat50710@gmail.com>
2026-06-08 22:49:15 +0200

    filtrage dynamique des freelances et validation du formulaire de contact

--- js/css/main.js
+++ js/css/main.js
@@ -123,3 +123,99 @@ const fadeObserver = new IntersectionObserver((entries) => {
 fadeElements.forEach(element => {
   fadeObserver.observe(element);
 });
+
+    //  ========filtrage dynamique des freelances======
+
+  function filterFreelancers(category){
+
+    const cards =
+    document.querySelectorAll(".freelancer");
+
+    cards.forEach(card => {
+
+        if(category === "all"){
+            card.style.display = "block";
+        }
+
+        else if(card.classList.contains(category)){
+            card.style.display = "block";
+        }
+
+        else{
+            card.style.display = "none";
+        }
+
+    });
+
+}
+
+
+     /* ============validation du formulaire de contact========== */
+
+     const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    document.getElementById("nameError").textContent = "";
+    document.getElementById("PrenomError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("messageError").textContent = "";
+    document.getElementById("successMessage").textContent = "";
+
+    let valid = true;
+
+    const name = document.getElementById("name").value.trim();
+    const prenom = document.getElementById("Prenom").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if(name === ""){
+        document.getElementById("nameError").textContent =
+        "Le nom est obligatoire";
+        valid = false;
+    }
+
+    if(prenom === ""){
+        document.getElementById("PrenomError").textContent =
+        "Le prénom est obligatoire";
+        valid = false;
+    }
+
+    const emailRegex =
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(email === ""){
+        document.getElementById("emailError").textContent =
+        "L'email est obligatoire";
+        valid = false;
+    }
+
+    else if(!emailRegex.test(email)){
+        document.getElementById("emailError").textContent =
+        "Email invalide";
+        valid = false;
+    }
+
+    if(message === ""){
+        document.getElementById("messageError").textContent =
+        "Le message est obligatoire";
+        valid = false;
+    }
+
+    else if(message.length < 20){
+        document.getElementById("messageError").textContent =
+        "Le message doit contenir au moins 20 caractères";
+        valid = false;
+    }
+
+    if(valid){
+        document.getElementById("successMessage").textContent =
+        "Message envoyé avec succès !";
+
+        form.reset();
+    }
+
+});
+     
